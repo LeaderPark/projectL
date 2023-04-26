b1a2ee12b8bbb99749570ba54b4eba4122e1b37d
sonhwanju <hwanjuson@gmail.com>
2023-04-26 14:43:44 +0900

    edit infomation.js

--- commands/components/infomation.js
+++ commands/components/infomation.js
@@ -5,6 +5,8 @@ module.exports = {
     .setName("정보")
     .setDescription("Replies with Pong!"),
   async execute(interaction) {
-    await interaction.reply("멍청한 머글들을 위해 마법사인 환주님이 친히 하사하신 인공두뇌");
+    await interaction.reply(
+      "멍청한 머글들을 위해 마법사인 진우님이 친히 하사하신 인공두뇌"
+    );
   },
 };

32f53b578f0dd2c0d4c9e1b0177e5347c1eea983
fitfunsJinWoo <dkdleldjqkr976@gmail.com>
2023-05-04 08:23:10 +0900

    팀 참가 제작

--- scripts/TeamData.js
+++ scripts/TeamData.js
@@ -1,13 +1,16 @@
 const attendTeam = (user) => {
-    console.log(user);
+  if (teamTable.includes(user.id)) return false;
+  teamTable.push(user.id);
+
+  return true;
 };
 
-const exitTeam = (user) => {
-
-};
+const exitTeam = (user) => {};
 
 const teamData = (userData, index) => {
+  console.log(teamTable);
+};
 
-}
+const teamTable = [];
 
-module.exports = {attendTeam, exitTeam}
+module.exports = { attendTeam, exitTeam, teamData };

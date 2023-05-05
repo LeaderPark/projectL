c4450a1f536db8a9ba6f3414197dcdab5b854b4a
LeaderPark <dkdleldjqkr976@gmail.com>
2023-05-05 17:33:02 +0900

    팀목록 기능 추가

--- commands/team/team.js
+++ commands/team/team.js
@@ -12,17 +12,17 @@ module.exports = {
       interaction.user.id
     );
     let text = "";
-
+      let index = 1;
     if (teamTable.length > 0) {
       await interaction.deferReply("searching...");
       for await (let v of teamTable) {
         const u = await interaction.guild.members.fetch(v);
-        text += `1: ${u}  \n`;
+        text += `${index++}번 소환사: ${u}\n`;
       }
     } else {
       return await interaction.reply("참가한 사람이 없습니다");
     }
 
-    await interaction.reply(text);
+    await interaction.editReply(text);
   },
 };

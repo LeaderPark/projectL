ef922b97fa6b944d38eda2c3a12ee55e0122941e
= <=>
2023-05-19 18:03:46 +0900

    서치 제작 중

--- commands/components/search.js
+++ commands/components/search.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, inlineCode } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { getUserData } = require("../../scripts/Utils/Query");
 
 const linesConvert = {
@@ -7,26 +7,30 @@ const linesConvert = {
   MID: "미드",
   BOT: "원딜",
   SUPPORT: "서폿",
-}
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("검색")
     .setDescription("원하는 소환사의 정보를 검색할 수 있어요.")
     .addUserOption((option) =>
-      option
-        .setName("검색할소환사")
-        .setDescription("검색할 소환사를 멘션해주세요")
+      option.setName("검색할소환사").setDescription("검색할 소환사를 멘션해주세요")
     ),
   async execute(interaction) {
     const interactionUser = await interaction.guild.members.fetch(
       interaction.user.id
     );
-
     const user = interaction.options.getUser("검색할소환사") || interactionUser;
-    if (user.bot) return await interaction.reply(`봇 말고 소환사를 넣으라고`);
+
+    if (user.bot) {
+      return await interaction.reply("봇 말고 소환사를 넣으라고");
+    }
+
     const result = await getUserData(user.id);
-    if (!result.success) return await interaction.reply(`오류가 발생했습니다.`);
+    if (!result.success) {
+      return await interaction.reply("오류가 발생했습니다.");
+    }
+
     const userData = result.data[0];
     const totalPlay = userData.win + userData.lose;
     const winRate = Math.floor((userData.win / totalPlay) * 100);
@@ -34,27 +38,31 @@ module.exports = {
     const totalDeath = (userData.t_death / totalPlay).toFixed(1);
     const totalAssist = (userData.t_assist / totalPlay).toFixed(1);
     const deathtoKillAssist = (
-      (Number(totalKill) + Number(totalAssist)) /
-      Number(totalDeath)
+      (Number(totalKill) + Number(totalAssist)) / Number(totalDeath)
     ).toFixed(2);
     const totalKillRate = (userData.t_kill_rate / totalPlay).toFixed(1);
-    const sortedLanes = Object.entries(JSON.parse(userData.lanes)).sort(([, a], [, b]) => {
-      const sumA = a.win + a.lose;
-      const sumB = b.win + b.lose;
-      return sumB - sumA;
-    });
-    const lines = Object.fromEntries(sortedLanes);
-    const linesKey = Object.keys(lines);
-    const mostLineTotal = lines[linesKey[0]].win + lines[linesKey[0]].lose;
-    const subLineTotal = lines[linesKey[1]].win + lines[linesKey[1]].lose;
-    const mostLineRate = Math.floor(lines[linesKey[0]].win / (mostLineTotal) * 100)
-    const subLineRate = Math.floor(lines[linesKey[1]].win / (subLineTotal) * 100)
 
-    const sortedFriends = Object.entries(JSON.parse(userData.friends)).sort(([, a], [, b]) => {
-      const sumA = (a.win / (a.win + a.lose)) * a.win;
-      const sumB = (b.win / (b.win + b.lose)) * b.win;
-      return sumB - sumA;
-    });
+    const sortedLanes = Object.entries(JSON.parse(userData.lanes)).sort(
+      ([, a], [, b]) => {
+        const sumA = a.win + a.lose;
+        const sumB = b.win + b.lose;
+        return sumB - sumA;
+      }
+    );
+    const [mostLine, subLine] = sortedLanes;
+    const mostLineTotal = mostLine[1].win + mostLine[1].lose;
+    const subLineTotal = subLine[1].win + subLine[1].lose;
+    const mostLineRate = Math.floor(
+      (mostLine[1].win / mostLineTotal) * 100
+    );
+    const subLineRate = Math.floor((subLine[1].win / subLineTotal) * 100);
+
+    const sortedFriends = Object.entries(JSON.parse(userData.friends)).sort(
+      ([, a], [, b]) => {
+        const sumA = (a.win / (a.win + a.lose)) * a.win;
+        const sumB = (b.win / (b.win + b.lose)) * b.win;
+        return sumB - sumA;
+      });
     const friends = Object.fromEntries(sortedFriends);
     const friendsKey = Object.keys(friends);
     const bestFriendTotal = friends[friendsKey[0]].win + friends[friendsKey[0]].lose;
@@ -95,7 +103,7 @@ module.exports = {
         },
         {
           name: `주라인 - ${mostLineTotal} Play`,
-          value: `**${linesKey[0]}** || **${mostLineRate}%**`,
+          value: `**${linesConvert[mostLine[0]]}** || **${mostLineRate}%**`,
           inline: true,
         },
         {
@@ -110,7 +118,7 @@ module.exports = {
         },
         {
           name: `부라인 - ${subLineTotal} Play`,
-          value: `**${linesKey[1]}** || **${subLineRate}%**`,
+          value: `**${linesConvert[subLine[0]]}** || **${subLineRate}%**`,
           inline: true,
         },
         {
@@ -132,7 +140,7 @@ module.exports = {
           name: `Worst Friend - ${worstFriendTotal} Play`,
           value: `**${friendsKey[friendsKey.length - 1]}** || **${worstFriendRate}%**`,
           inline: true,
-        },
+        }
       )
       .setTimestamp()
       .setFooter({

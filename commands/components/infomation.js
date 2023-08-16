10b23703fc760401c3a592afc9fc1144dbf216ae
= <=>
2023-08-16 12:04:14 +0900

    정보 수정

--- commands/components/infomation.js
+++ commands/components/infomation.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("정보")
-    .setDescription("머글봇을 관음할 수 있어요."),
+    .setDescription("머글봇의 정보를 볼 수 있어요."),
   async execute(interaction) {
     await interaction.reply(`머글들의 피난처 내전 봇 V154`);
   },

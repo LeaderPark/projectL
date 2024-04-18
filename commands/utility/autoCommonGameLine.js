bf03b6028afa52c7c9333e241c2fa5699b880655
fitfunsJinWoo <jwpark@fitfuns.com>
2024-04-18 15:45:58 +0900

    일겜

--- commands/utility/autoCommonGameLine.js
+++ commands/utility/autoCommonGameLine.js
@@ -3,17 +3,20 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("일겜")
-    .setDescription("일반 게임 라인을 정해줘요."),
+    .setDescription("사용자 이름을 랜덤하게 섞어서 반환합니다.")
+    .addStringOption((option) =>
+      option
+        .setName("이름들")
+        .setDescription(
+          "사람 최대 5명의 이름을 입력하세요. 이름은 쉼표로 구분합니다."
+        )
+        .setRequired(true)
+    ),
   async execute(interaction) {
-    const msg = await interaction.reply({
-      content: "Pong!",
-      fetchReply: true,
-    });
-
-    await interaction.editReply({
-      content: `Pong!\nBot Latency: ${
-        msg.createdTimestamp - interaction.createdTimestamp
-      }ms`,
-    });
+    const namesString = interaction.options.getString("이름들");
+    const namesArray = namesString.split(",").map((name) => name.trim());
+    const shuffledArray = namesArray.sort(() => Math.random() - 0.5);
+    console.log(shuffledArray);
+    // await interaction.reply(shuffledArray.join(", "));
   },
 };

19e5eb708e8e195fb019a142a6800f3452921a29
fitfunsJinWoo <jwpark@fitfuns.com>
2024-04-23 11:05:42 +0900

    alarm

---
+++ commands/utility/alarm.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("알림")
+    .setDescription("Provides information about the user.")
+    .addStringOption((option) =>
+      option.setName("내용").setDescription("보낼 내용을 작성해주세요.")
+    ),
+  async execute(interaction) {
+    const notiString = interaction.options.getString("내용");
+    const user = await client.users.fetch(interaction.user.id);
+    user
+      .send(notiString)
+      .then(() => console.log("메시지가 성공적으로 전송되었습니다."))
+      .catch(console.error); // 오류 처리
+  },
+};

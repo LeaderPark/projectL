281ffc45af31caffa75813e5428999effe1381e1
fitfunsJinWoo <jwpark@fitfuns.com>
2024-04-23 11:26:53 +0900

    alarm fix

--- commands/utility/alarm.js
+++ commands/utility/alarm.js
@@ -3,18 +3,32 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("알림")
-    .setDescription("Provides information about the user.")
+    .setDescription("특정 역할을 가진 사용자에게 정보를 제공합니다.")
+    .addRoleOption((option) =>
+      option
+        .setName("역할")
+        .setDescription("메시지를 받을 역할을 선택해주세요.")
+        .setRequired(true)
+    )
     .addStringOption((option) =>
-      option.setName("내용").setDescription("보낼 내용을 작성해주세요.")
+      option
+        .setName("내용")
+        .setDescription("보낼 내용을 작성해주세요.")
+        .setRequired(true)
     ),
   async execute(interaction) {
     const notiString = interaction.options.getString("내용");
-    const interactionUser = await interaction.guild.members.fetch(
-      interaction.user.id
-    );
-    interactionUser
-      .send(notiString)
-      .then(() => console.log("메시지가 성공적으로 전송되었습니다."))
-      .catch(console.error); // 오류 처리
+    const role = interaction.options.getRole("역할");
+
+    role.members.forEach((member) => {
+      if (!member.user.bot) {
+        member
+          .send(notiString)
+          .then(() => console.log(`Message sent to ${member.user.tag}`))
+          .catch(console.error);
+      }
+    });
+
+    await interaction.reply("메시지가 성공적으로 전송되었습니다.");
   },
 };
